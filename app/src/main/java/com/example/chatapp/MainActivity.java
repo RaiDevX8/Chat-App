13c0087a20e597df1a91201ed9f3b23cb7224287
ayushwagh2 <ayushwagh2002@gmail.com>
2024-10-16 22:53:08 +0530

    settings changes

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.chatapp.activity.main_chat_app;
 import com.example.chatapp.activity.loginphone;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,14 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
-        setContentView(R.layout.splashscreen);
+        setContentView(R.layout.splashscreen); // Replace with your actual splash screen layout
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, loginphone.class));
+                // Check if the user is already logged in
+                if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                    // User is logged in, navigate to Userprofile activity
+                    startActivity(new Intent(MainActivity.this, main_chat_app.class));
+                } else {
+                    // User is not logged in, navigate to loginphone activity
+                    startActivity(new Intent(MainActivity.this, loginphone.class));
+                }
                 finish();
             }
-        },3000);
-
+        }, 3000);
     }
 }
