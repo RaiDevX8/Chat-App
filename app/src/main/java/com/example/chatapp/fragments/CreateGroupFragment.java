85278fc1bf93429d93b9545f42c4781cff6f0c01
RaiDevX8 <charanrai193@gmail.com>
2024-10-17 17:28:32 +0530

    ADD : group chat back end

--- app/src/main/java/com/example/chatapp/fragments/CreateGroupFragment.java
+++ app/src/main/java/com/example/chatapp/fragments/CreateGroupFragment.java
@@ -6,12 +6,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapp.R;
 import com.example.chatapp.activity.CreateGroupActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateGroupFragment extends Fragment {
 
